ade8061d3b165c24496ff6b5fdeb1efd0eb1efab
Vivek Kumar Pandey <vs9595@srmist.edu.in>
2024-01-10 08:07:20 +0530

    package-json Update

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ app.use(bodyParser.json());
 
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(process.env.MONGO_URI);
 
   // In-memory data store
   const post = new mongoose.Schema({
